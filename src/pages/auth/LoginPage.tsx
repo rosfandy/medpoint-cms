fbb0cb1c8641c04717f01bc4a9b6ece70d252d91
rosfandy <brosfandy@gmail.com>
2024-12-12 10:35:10 +0700

    add feat login page

---
+++ src/pages/auth/LoginPage.tsx
@@ -0,0 +1,10 @@
+import { LoginForm } from "../../components/form";
+
+export function Login() {
+  return (
+    <div className="">
+      <div className="">Login Page</div>
+      <LoginForm />
+    </div>
+  );
+}
